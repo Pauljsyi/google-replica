6501df476c56afcdb1252502bb7dc51f9b205d0e
Paul Yi <pauljsyi@gmail.com>
2020-11-20 08:11:56 -0800

    centered logo & set header to 100% width and height for Responsive Design

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
- 
+const randomizeButton = document.getElementById('randomizeButton')
+
+function randomizeText(idElement){
+    if(idElement==1){
+        document.getElementById('element'+idElement).innerHTML ='<a href="http://www.google.com">Google</a>';
+    } else if(idElement==2){
+        document.getElementById('element'+idElement).innerHTML ='<a href="http://woork.blogspot.com">Woork</a>';
+    }
+}
+
+// randomizeButton.addEventListener('')
